0e04b4ad222d2f0831434d06c5185933a598e296
Indra Nurwibisono <bigmasterindra@yahoo.com>
2021-12-03 20:45:42 +0700

    chhange dj db url

--- portfolio3/settings.py
+++ portfolio3/settings.py
@@ -89,8 +89,10 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 
