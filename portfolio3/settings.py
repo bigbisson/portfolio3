0c70fb0f3b5cc5865211463fc63a7db8d1adf16a
Indra Nurwibisono <bigmasterindra@yahoo.com>
2021-12-03 20:42:07 +0700

    change static and media url and root

--- portfolio3/settings.py
+++ portfolio3/settings.py
@@ -150,12 +150,12 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = "/static/"
-MEDIA_URL = "/media/"
-
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
