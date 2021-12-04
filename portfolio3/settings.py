55fbe3aa6e280dd5ec1754cc426e201e47f6ee57
Indra Nurwibisono <bigmasterindra@yahoo.com>
2021-12-04 12:48:53 +0700

    Change Setting for change key

--- portfolio3/settings.py
+++ portfolio3/settings.py
@@ -94,10 +94,10 @@ DATABASES = {
     }
 }
 
-# db_from_env = dj_database_url.config()
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
